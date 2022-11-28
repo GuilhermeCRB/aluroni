4ce5dee667685bca56dd5e2445afd9c44fdabba3
GuilhermeCRB <guilhermecrbarros@gmail.com>
2022-11-28 20:28:47 -0300

    feat: displays items

---
+++ src/pages/Carte/ItemsList/index.tsx
@@ -0,0 +1,13 @@
+import carte from './items.json';
+import Item from './Item';
+import styles from './Items.module.scss';
+
+export default function ItemsList() {
+  return (
+    <div className={styles.items}>
+      {carte.map((item) => (
+        <Item key={item.id} />
+      ))}
+    </div>
+  );
+}
--- src/pages/Carte/index.tsx
+++ src/pages/Carte/index.tsx
@@ -6,6 +6,7 @@ import Header from 'components/Header';
 import SearchBar from './SearchBar';
 import Filters from './Filters';
 import OrderBy from './OrderBy';
+import ItemsList from './ItemsList';
 
 export default function Carte() {
   const [search, setSearch] = useState('');
@@ -21,10 +22,11 @@ export default function Carte() {
       <section className={styles.carte}>
         <h3 className={styles.carte__title}>Cardápio</h3>
         <SearchBar search={search} setSearch={setSearch} />
-        <div className={styles.cardapio__filters}>
+        <div className={styles.carte__filters}>
           <Filters filter={filter} setFilter={setFilter} />
           <OrderBy orderBy={orderBy} setOrderBy={setOrderBy} />
         </div>
+        <ItemsList />
       </section>
     </main>
   );
