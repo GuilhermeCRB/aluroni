9de75a89f9f4826fb0f71c0f6b7e73e42497ff23
GuilhermeCRB <guilhermecrbarros@gmail.com>
2022-11-28 10:47:33 -0300

    feat:©reates search area

---
+++ src/pages/Carte/SearchBar/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './SearchBar.module.scss';
+import { CgSearch } from 'react-icons/cg';
+
+interface Props {
+  busca: string;
+  setBusca: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export default function SearchBar({ busca, setBusca }: Props) {
+  return (
+    <div className={styles.searchbar}>
+      <input value={busca} onChange={(evento) => setBusca(evento.target.value)} />
+      <CgSearch size={20} color="#4C4D5E" />
+    </div>
+  );
+}
--- src/pages/Carte/index.tsx
+++ src/pages/Carte/index.tsx
@@ -1,14 +1,23 @@
-import styles from './Carte.module.scss';
+import { useState } from 'react';
+
 import { ReactComponent as Logo } from 'assets/logo.svg';
+import styles from './Carte.module.scss';
 import Header from 'components/Header';
+import SearchBar from './SearchBar';
 
 export default function Carte() {
+  const [busca, setBusca] = useState('');
+
   return (
     <main>
-      <nav className={styles.carte}>
+      <nav className={styles.menu}>
         <Logo />
       </nav>
       <Header />
+      <section className={styles.carte}>
+        <h3 className={styles.carte__title}>Cardápio</h3>
+        <SearchBar busca={busca} setBusca={setBusca} />
+      </section>
     </main>
   );
 }
