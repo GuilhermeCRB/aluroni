ae5bb9b173a5d5a878d09a5a12be7f28c5d818be
GuilhermeCRB <guilhermecrbarros@gmail.com>
2022-11-29 21:41:26 -0300

    feat: adds back button on page 404

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,11 +1,18 @@
+import { useNavigate } from 'react-router-dom';
+
 import { ReactComponent as NotFoundImage } from 'assets/NotFound/not_found.svg';
 import stylesTheme from 'styles/Theme.module.scss';
 import styles from './NotFound.module.scss';
 import classNames from 'classnames';
 
 export default function NotFound() {
+  const navigate = useNavigate();
+
   return (
     <div className={classNames([styles.container], [stylesTheme.container])}>
+      <div className={styles.back}>
+        <button onClick={() => navigate(-1)}>{'< Voltar'}</button>
+      </div>
       <NotFoundImage />
     </div>
   );
